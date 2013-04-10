7b0eb782847a4f5fe82d97f779becd7b2e662d2e
Dillon Young <info@dillonyoung.ca>
2013-04-10 08:56:51 -0400

    Text Cleanup
    - Cleaned up the description on the management pages

--- SimpleCMS/manage-home.php
+++ SimpleCMS/manage-home.php
@@ -7,6 +7,10 @@
 	 */	
 ?>
 <h1>Home</h1>
-<p>Welcome to the management console for the Simple CMS. From this page you can create, modify, or delete posts.</p>
-<p>This management console also allows you to manage user account access by locking or unlocking accounts.</p>
+<p>Welcome to the management console for the Simple CMS. This management console allows you to manage the functionality of the site.</p>
+<p>The management console is broken up into four main sections which include Site, Posts, Users, and About.</p>
+<p>The Site section will allow you to modify the site title and description.</p>
+<p>From the posts section you will be able to create, modify, or delete posts.</p>
+<p>The users section will allow you to manage user account access by locking or unlocking accounts.</p>
+<p>Finally the about section will display information regarding the installed modules.</p>
 <p>Each section of the management console has more information.</p>
--- SimpleCMS/manage-site.php
+++ SimpleCMS/manage-site.php
@@ -7,7 +7,7 @@
 	 */	
 ?>
 <h1>Site</h1>
-<p>Use the below fields to update the site title and site description.</p>
+<p>Use the below fields to update the site title and site description. The site title is displayed in the page header as well as the top of each page. The site description is displayed below the site header.</p>
 
 <label for="txt_manage_sitetitle">Site Title:</label>
 <input type="text" name="txt_manage_sitetitle" id="txt_manage_sitetitle" />
